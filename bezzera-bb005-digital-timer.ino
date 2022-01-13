ee829279a9c9413453eac8297f123e9402664a5a
Kenneth Lee <60289339+kenleejl@users.noreply.github.com>
2022-01-14 04:50:21 +0800

    Changed Display from SH1106 to SSD1306

--- bezzera-bb005-digital-timer.ino
+++ bezzera-bb005-digital-timer.ino
@@ -13,7 +13,7 @@
 #include <Wire.h>
 #include <sstream>
 
-U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 
 //===============================================================
 //SSID and Password to connect to your ESP Access Point
